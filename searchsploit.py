51ac468ff8ba5237df7dd9e6ef08b1ec900c7bbf
vanta <khoeric1@yahoo.co.id>
2022-12-20 06:05:24 -0500

    Add is verbose

--- searchsploit.py
+++ searchsploit.py
@@ -1,21 +1,9 @@
 import basic_command
 
-def scan(service_target, current_time):
+def scan(service_target, current_time, is_verbose):
     output = f"searchsploit_{target}_{current_time}.txt"
     output_directory = f"./result/current_time/searchsploit/"
 
     basic_command.mkdir(output_directory)
 
-    basic_command.run_command(f"searchsploit {service_target} > {output_directory}{output}")
-
-def show_options(target, current_time, is_verbose, is_run):
-    options = f"""
-    searchsploit
-    ---
-
-    TARGET          {target}
-    """
-    print(options)
-
-    if is_run:
-        scan(target, current_time, is_verbose)
+    basic_command.run_command(f"searchsploit {service_target} {basic_command.verbose_level(is_verbose)} {output_directory}{output}")
--- wpscan.py
+++ wpscan.py
@@ -1,21 +1,9 @@
 import basic_command
 
-def scan(target, current_time):
+def scan(target, current_time, is_verbose):
     output = f"wpscan_{target}_{current_time}.txt"
     output_directory = f"./result/current_time/wpscan/"
 
     basic_command.mkdir(output_directory)
 
-    basic_command.run_command(f"wpscan --url {target} -e > {output_directory}{output}")
-
-def show_options(target, current_time, is_verbose, is_run):
-    options = f"""
-    wpscan
-    ---
-
-    TARGET          {target}
-    """
-    print(options)
-
-    if is_run:
-        scan(target, current_time, is_verbose)
+    basic_command.run_command(f"wpscan --url {target} -e {basic_command.verbose_level(is_verbose)} {output_directory}{output}")
