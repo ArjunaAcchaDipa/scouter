d82394f92870309a5bc274be6edc5846fb0077af
ArjunaAcchaDipa <arjunaacchadipa@gmail.com>
2022-12-26 23:44:34 +0700

    Add scan module for nmap

---
+++ nmap.py
@@ -0,0 +1,16 @@
+import basic_command
+
+def scan(target, current_time, is_verbose):
+    output = f"nmap_{target}_{current_time}.txt"
+    output_directory = f"./result/current_time/nmap/"
+
+    basic_command.mkdir(output_directory)
+
+    # -A        --> Enable OS detection, version detection, script scanning, and traceroute
+    # -p-      --> Port range 1-65535
+    # --script  --> script scan
+    # vulners   --> scripts to scan vulnerability
+    # -T4       --> timing template set to 4 (higher is faster)
+    # -Pn       --> Treat all hosts as online (skip host discovery)
+
+    basic_command.run_command(f"nmap -A -p- --script vulners -T4 -Pn {basic_command.verbose_level(is_verbose)} {output_directory}{output}")
