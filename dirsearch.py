5a597a2c26be411ca848ba5acc403daf1dc42618
delvinnicholas <delvin.nicholas@binus.ac.id>
2023-01-08 20:48:30 +0700

    remove wordlist, add extension & exclude status-code

--- dirsearch.py
+++ dirsearch.py
@@ -1,31 +1,31 @@
 import basic_command
 
-def scan(target, thread, is_default, current_time, wordlist, is_verbose):
+def scan(target, thread, is_default, current_time, is_verbose):
     output_file= f"dirsearch_{target}.txt"
     output_directory = f"./result/{current_time}/"
     output = f"{output_directory}{output_file}"
 
     basic_command.mkdir(output_directory)
     
-    wordlist = default_check(is_default, wordlist)
+    thread = default_check(is_default, thread)
 
     # -u    --> target's url/ip
-    # -w    --> wordlists
     # -t    --> threads
     # -r    --> recursive
+    # -e    --> extension
+    # -x    --> exclude status-code
 
-    basic_command.run_command(f"dirsearch -u {target} -w {wordlist} -t {thread} -r {basic_command.verbose_level(is_verbose)} {output}")
+    basic_command.run_command(f"dirsearch -u {target} -e php,html,js,txt,sql -t {thread} -r -x 402-999 {basic_command.verbose_level(is_verbose)} {output}")
 
     result = basic_command.read_file(f"{output}")
 
     return f"{result}"
 
-def default_check(is_default, wordlist):
-    default_wordlist = "./wordlist/directory-list-2.3-medium.txt"
-
-    if is_default:
-        wordlist = default_wordlist
-    elif wordlist == "":
-        wordlist = input("Wordlist for dirsearch: ")
+def default_check(is_default, thread):
     
-    return wordlist
+    if is_default:
+        thread = "50" 
+    elif wordlist == "":
+        thread = input("Thread: for dirsearch: ")
+    
+    return thread
